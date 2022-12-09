6d069c7f25e6ba6c3afa4590a47fbcb8dfd2a2a8
Said Mazaya <117622294+saidmazaya@users.noreply.github.com>
2022-12-09 12:22:06 +0700

    ngubah

---
+++ src/components/AddTaskForm.jsx
@@ -0,0 +1,30 @@
+const AddTaskForm = ({ newTask, setNewTask, addTask }) => {
+  return (
+    <>
+      {/* Add Task */}
+      <div className="row">
+        <div className="col">
+          <input
+            value={newTask}
+            placeholder="Add Task"
+            onChange={(e) => setNewTask(e.target.value)}
+            className="form-control form-control-lg"
+            onKeyDown={(event) => {
+              if (event.key === "Enter") {
+                addTask(newTask);
+              }
+            }}
+          />
+        </div>
+        <div className="col-auto">
+          <button onClick={addTask} className="btn btn-lg btn-success">
+            Add Task
+          </button>
+        </div>
+      </div>
+      <br />
+    </>
+  );
+};
+
+export default AddTaskForm;
